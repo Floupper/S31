6b8ee662ba345491db3df8d85bf9157478e9161d
Mathieu Ackermann <mathieu.ackermann@etu.unistra.fr>
2023-10-19 14:19:26 +0200

    Add Advancement TP pipe

--- TD-TP4/td4_ex2.2.2.c
+++ TD-TP4/td4_ex2.2.2.c
@@ -9,21 +9,24 @@
 int main(int argc, char *argv[])
 {
 
-    char c;
+    char c[100];
+    for (int i = 1; i < argc; i++)
+    {
+        c[i] = argv[i];
+    }
+
     int fd[2]; // Pipe entre le père et le fils
 
     // Création du pipe
     pipe(fd);
     if (fork() == 0)
     {
-        c = 'a';
-        write(fd, &c, 1);
+        write(fd[1], &c, 1);
         printf("Caractère écrit par le fils : %s \n", c);
     }
     else
     {
-        read(fd, &c, 1);
+        read(fd[0], &c, 1);
         printf("Caractère lu par le père : %s \n", c);
     }
 }
-\
